1efc8875a4be69e09d05648d26f9b3ff5e9040ee
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-06 23:09:28 +0300

    feat(exerciese): add exercise #10

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 // Write Your Code Here
-type mix = number | boolean;
+type Info = {
+  theName: string;
+  theAge: number;
+};
+
+type Full = Info & {
+  country: string;
+};
+
 // Do Not Edit Here
-let myInfo: mix;
-myInfo = 1000; // No Problem Here
-console.log(myInfo);
-myInfo = true; // No Problem Here
-console.log(myInfo);
+function showInfo(data: Info) {
+  console.log(`The Name Is ${data.theName}`);
+  console.log(`The Age Is ${data.theAge}`);
+}
+console.log(showInfo({ theName: "Elzero", theAge: 40 }));
+
+function showFullInfo(data: Full) {
+  console.log(`The Name Is ${data.theName}`);
+  console.log(`The Age Is ${data.theAge}`);
+  console.log(`The Country Is ${data.country}`);
+}
+console.log(showFullInfo({ theName: "Elzero", theAge: 4, country: "Egypt" }));
