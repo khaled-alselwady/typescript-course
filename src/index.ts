dfa3357e8ee0621bb45ae8399f2a1ef85d572f51
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-06 23:33:40 +0300

    feat(exerciese): add exercise #15

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-// Create Enums + Function Here
-function getInsaneScore(num: number) {
-  return num - Game.Hard;
-}
-enum Game {
-  Easy = 100,
-  Medium = Easy - 20,
-  Hard = Medium - Easy / 2,
-  Insane = getInsaneScore(50),
-}
-// Output
-console.log(Game.Easy); // 100
-console.log(Game.Medium); // 80
-console.log(Game.Hard); // 30
-console.log(Game.Insane); // 20
+const user: {
+  username: string;
+  age: number | string;
+  website?: string;
+  skills: {
+    frontEnd: string[];
+    backEnd: (string | number)[];
+  };
+} = {
+  username: "Elzero",
+  age: 40,
+  website: "Elzero.org",
+  skills: {
+    frontEnd: ["HTML", "CSS", "JS"],
+    backEnd: ["PHP", "Python"],
+  },
+};
+
+// We Need To Remove Error From This Edits
+user.username = "Osama";
+user.age = "40";
+user.skills.backEnd.push(100);
