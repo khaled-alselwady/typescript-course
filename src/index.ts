4a806788632b874b105e01193a4e8c6865f962dd
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-07 17:36:01 +0300

    feat(exerciese): add exercise #22

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-class Show {
-  constructor(private _title: string) {}
+interface Play {
+  id: number;
+  title: string;
+  level: number | string;
+  logIn(): void;
+  logOut(msg: string): void;
+}
 
-  public get title() {
-    return this._title;
+class Player implements Play {
+  constructor(
+    public id: number,
+    public title: string,
+    public level: number | string
+  ) {}
+
+  logIn() {
+    console.log("Logged In");
   }
 
-  public set title(value: string) {
-    this._title = value;
+  logOut(msg: string) {
+    console.log(`Logged Out, ${msg}`);
   }
 }
 
-let tester = new Show("Elzero");
+// Create Your Class Here
 
-console.log(tester.title); // Property 'title' does not exist on type 'Show'
-tester.title = "Osama"; // Property 'title' does not exist on type 'Show'
-console.log(tester.title); // Property 'title' does not exist on type 'Show'
+let player1 = new Player(100, "Elzero", 95);
+
+console.log(player1.id); // 100
+console.log(player1.title); // "Elzero"
+console.log(player1.level); // 95
+player1.logIn(); // Logged In
+player1.logOut("Good Bye"); // Logged Out, Good Bye
