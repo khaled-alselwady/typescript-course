00230ee507f5ff3e9e7c1632996c51f3fa8cf76c
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-07 17:24:12 +0300

    feat(exerciese): add exercise #19

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
+// Create Class Here
+class Player {
+  firstName: string;
+  lastName: string;
+  level: number | string;
+  state?: boolean;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    level: number | string,
+    state?: boolean
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.level = level;
+    this.state = state;
+  }
+
+  details(): string {
+    return `${
+      this.state ? `VIP ${this.firstName}` : `${this.firstName}`
+    }, Type is ${this.lastName} Level is ${this.level}`;
+  }
+}
 // Do Not Edit The Code Below
-interface Man {
-  title: string;
-  weight: number;
-  age: number;
-}
+let player1 = new Player("Osama", "Mage", 90, true);
+let player2 = new Player("Shady", "Archer", "85", false);
+let player3 = new Player("Amr", "Fighter", 50, true);
+let player4 = new Player("Mahmoud", "Assassin", 77);
 
-interface Bird {
-  canFly: boolean;
-}
-
-interface Superman extends Man, Bird {
-  bodyType: string;
-  origin: string;
-}
-
-let creature: Superman = {
-  title: "Superman",
-  weight: 100,
-  age: 500,
-  canFly: true,
-  bodyType: "Iron",
-  origin: "Krypton",
-};
+console.log(player1.details()); // VIP Osama, Type Is Mage Level Is 90
+console.log(player2.details()); // Shady, Type Is Archer Level Is 85
+console.log(player3.details()); // VIP Amr, Type Is Fighter Level Is 50
+console.log(player4.details()); // Mahmoud, Type Is Assassin Level Is 77
