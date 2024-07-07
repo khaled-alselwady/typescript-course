4e5c84c1502edff63fbfb2b2a44c045941ff1c4d
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-07 17:06:37 +0300

    feat(exerciese): add exercise #17

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
-// Edit The Interface To Fix The Problems
-interface Member {
-  id: number | string;
+// Create Interface Here
+interface Client {
+  id: number;
   username: string;
-  country: string;
-  state: boolean;
-  // getName: () => string;
-  getName(): string;
+  active: boolean;
+  discount: number;
+  // getPrice: (price: number) => number;
+  getPrice(price: number): number;
 }
-
 // Do Not Edit The Code Below
-let user: Member = {
-  // Property 'country' is missing in type
+let client: Client = {
   id: 100,
   username: "Elzero",
-  country: "Amman",
-  state: true,
-  getName() {
-    // 'getName' does not exist in type 'Member'
-    return this.username;
+  active: true,
+  discount: 10,
+  getPrice(price: number) {
+    return price - this.discount;
   },
 };
 
-user.id = 200;
-user.id = "200"; // Type 'string' is not assignable to type 'number'
-user.state = false; // Cannot assign to 'state' because it is a read-only property
+console.log(`Client Id Is ${client.id}`);
+console.log(`Client Name Is ${client.username}`);
+console.log(`Client Has Discount ${client.discount}`);
+console.log(`Client Product After Discount Is ${client.getPrice(200)}`);
