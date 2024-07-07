f60ac102a015cf173308f353c8af4303990eb6fb
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-07 17:26:09 +0300

    feat(exerciese): add exercise #20

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,16 @@
-// Create Class Here
-class Player {
-  firstName: string;
-  lastName: string;
-  level: number | string;
-  state?: boolean;
-
+class Shorten {
   constructor(
-    firstName: string,
-    lastName: string,
-    level: number | string,
-    state?: boolean
+    public id: number,
+    public username: string,
+    protected title: string
   ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.level = level;
-    this.state = state;
-  }
-
-  details(): string {
-    return `${
-      this.state ? `VIP ${this.firstName}` : `${this.firstName}`
-    }, Type is ${this.lastName} Level is ${this.level}`;
+    this.id = id;
+    this.username = username;
+    this.title = title;
   }
 }
-// Do Not Edit The Code Below
-let player1 = new Player("Osama", "Mage", 90, true);
-let player2 = new Player("Shady", "Archer", "85", false);
-let player3 = new Player("Amr", "Fighter", 50, true);
-let player4 = new Player("Mahmoud", "Assassin", 77);
 
-console.log(player1.details()); // VIP Osama, Type Is Mage Level Is 90
-console.log(player2.details()); // Shady, Type Is Archer Level Is 85
-console.log(player3.details()); // VIP Amr, Type Is Fighter Level Is 50
-console.log(player4.details()); // Mahmoud, Type Is Assassin Level Is 77
+let tester = new Shorten(100, "Elzero", "Developer");
+
+console.log(tester.id);
+console.log(tester.username);
